ef328b882aa7c87c0754cba653247d70f1157ca2
UD <46685886+black-child-cat@users.noreply.github.com>
2024-01-07 23:27:32 +0900

    特定の動的ページのパスを静的にエクスポートする

--- next.config.js
+++ next.config.js
@@ -6,5 +6,26 @@ module.exports = {
   // GitHub Pages用に設定
   assetPrefix: isProd ? "/meaningful-talk/" : "",
   basePath: isProd ? "/meaningful-talk" : "",
+  exportPathMap: async function () {
+    const paths = {
+      "/": { page: "/" },
+      "/chat": { page: "/chat" },
+      // 他の静的ページ...
+    };
+
+    // 予め定義されたユーザーIDのリスト
+    const userIds = ["01", "02", "03", "04", "05", "06", "07"];
+
+    // 各ユーザーIDに対してパスを生成
+    userIds.forEach((userId) => {
+      paths[`/chat/${userId}`] = { page: "/chat/[userId]", query: { userId } };
+      paths[`/chat/${userId}/user`] = {
+        page: "/chat/[userId]/user",
+        query: { userId },
+      };
+    });
+
+    return paths;
+  },
   output: "export",
 };
