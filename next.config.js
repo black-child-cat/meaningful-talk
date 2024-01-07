548e5d422eac0cf0ae2fce3f046d8416319cd77b
UD <46685886+black-child-cat@users.noreply.github.com>
2024-01-07 23:22:56 +0900

    静的エクスポートの方法を検証

--- next.config.js
+++ next.config.js
@@ -6,12 +6,5 @@ module.exports = {
   // GitHub Pages用に設定
   assetPrefix: isProd ? "/meaningful-talk/" : "",
   basePath: isProd ? "/meaningful-talk" : "",
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-      // 他のページも同様に追加
-    };
-  },
   output: "export",
 };
