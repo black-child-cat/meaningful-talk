a6860d54a49871358d3fe6202349a1e7335113dd
UD <46685886+black-child-cat@users.noreply.github.com>
2024-01-10 03:35:50 +0900

    インデックスページ変更

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 // components/Header.js
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  const isIndexPage = router.pathname === "/";
+  console.log(isIndexPage);
   return (
-    <header className="bg-white shadow-sm border-b border-gray-100">
+    <header
+      className={`${
+        isIndexPage ? "" : "shadow-sm border-b border-gray-100"
+      } bg-white`}
+    >
       <div className="max-w-lg mx-auto flex">
         <Link href="/" className="text-center mx-auto">
           <span className="c-title text-5xl text-gray-800 inline-block align-[0.5rem]">
